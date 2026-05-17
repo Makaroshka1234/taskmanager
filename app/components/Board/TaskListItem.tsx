24353dd4dea5f1470f4a3d5bbdfa2574c5142f5a
Maksym Tutyk <makstutik007@gmail.com>
2026-05-18 02:46:01 +0300

    change item ui

--- app/components/Board/TaskListItem.tsx
+++ app/components/Board/TaskListItem.tsx
@@ -1,23 +1,59 @@
 import { Dialog, DialogTrigger } from "@/schadComponents/ui/dialog";
-import { Plus } from "lucide-react";
+import { DeleteIcon, Plus, TrashIcon } from "lucide-react";
 import EditListItemPopUp from "./EditListItemPopUp";
 import { Button } from "@/schadComponents/ui/button";
+import { Input } from "@/schadComponents/ui/input";
+import { Checkbox } from "@/schadComponents/ui/checkbox";
+import { Badge } from "@/schadComponents/ui/badge";
+import { useState } from "react";
 
 interface TaskListItemProps {
   id: string;
   title: string;
   priority: string;
 }
-
+const priorityStyles: Record<string, string> = {
+  HIGH: "bg-red-50 text-red-700 dark:bg-red-950 dark:text-red-300",
+  MEDIUM:
+    "bg-yellow-50 text-yellow-700 dark:bg-yellow-950 dark:text-yellow-300",
+  LOW: "bg-green-50 text-green-700 dark:bg-green-950 dark:text-green-300",
+};
 function TaskListItem(props: TaskListItemProps) {
+  const [isComplete, setIsComplete] = useState<boolean>(false);
   const { title, priority } = props;
+
+  function deleteListItem(id: string) {}
   return (
     <>
       <EditListItemPopUp>
-        <Button>{title}</Button>
+        <li className="flex items-center justify-between border py-3 px-3 cursor-pointer hover:bg-muted/50 transition rounded-xl">
+          <div className="flex gap-3">
+            <Checkbox
+              onClick={(e) => e.stopPropagation()}
+              checked={isComplete}
+              onCheckedChange={(checked) => {
+                setIsComplete(checked === true);
+              }}
+            />
+
+            <p>{title}</p>
+          </div>
+          <div className="flex gap-2 items-center">
+            <Badge className={priorityStyles[priority]}>{priority}</Badge>
+            {isComplete && (
+              <Button
+                type="button"
+                size="icon-xs"
+                variant="destructive"
+                onClick={(e) => e.stopPropagation()}
+              >
+                <TrashIcon height={10} />
+              </Button>
+            )}
+          </div>
+        </li>
       </EditListItemPopUp>
-      <p>{priority}</p>
-    </> 
+    </>
   );
 }
 
